46e99b6f1bfd7e4fca44311354a9e8220cbe71d0
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2024-04-16 12:30:12 +0000

    Rebase

---
+++ build/precache-manifest.a7687e5cbe2d47798c71e439b84adf47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a257d59855b86a898e85a9e44ae50a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd05b28db17ce01e7f1",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "eff959bbbcdec35c2b21",
+    "url": "/static/js/2.a512e5b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a512e5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd05b28db17ce01e7f1",
+    "url": "/static/js/main.a82dcab8.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "f091ed46c568a1814b98947f69048059",
+    "url": "/static/media/logo.f091ed46.png"
+  }
+]);
